db47503ffff75a55ca8dc6e4c29e0d4c06a88575
Aditya Kumar Singh <code280207@gmail.com>
2026-08-27 19:46:32 +0530

    Update 344_reverse_string.cpp - LeetCode Problem 344

--- 344_reverse_string.cpp
+++ 344_reverse_string.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+
         int i=0;
         int j=s.size()-1;
-        while(i<j){
-            swap(s[i++],s[j--]);
+
+        while(i<=j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+    
         }
+
+
+
         
     }
 };
