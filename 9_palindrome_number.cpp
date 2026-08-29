22377a77bce1f5a276f02e60ea88b675eda761e8
Aditya Kumar Singh <code280207@gmail.com>
2026-08-29 12:35:21 +0530

    Add 9_palindrome_number.cpp - LeetCode Problem 9

---
+++ 9_palindrome_number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int st =0;
+        int e = s.length()-1;
+        while(st<=e){
+            if(s[st]!=s[e]){
+                return false;
+            }
+            else{
+                st++;
+                e--;
+            }
+            
+            
+        }
+        return true;
+        
+    }
+};
